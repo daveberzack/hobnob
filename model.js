bb377e8d5623678087aa78acd583ff7e70fc4f41
Dave Berzack <dberzack>
2015-03-27 13:58:02 -0400

    a

--- model.js
+++ model.js
@@ -235,7 +235,7 @@ document.addEventListener("deviceready", function(){ model.init() }, false);
 
 //  ======================================== HELPERS ========================================
 
-var inBrowser = true;
+var inBrowser = false;
 var autoMode = "";//empty,start,init,win
 var debugMode = "console";//popup, console, none
 
